4b8a64b6b868f3d44d2ffc56efc2229d96ea3b16
Alen <jackoptimist@gmail.com>
2023-12-06 19:30:00 +0600

    For Zhalgas

--- app/src/main/java/com/example/chartapp/MainActivity.kt
+++ app/src/main/java/com/example/chartapp/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.chartapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,33 +16,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val pieChart: PieChart = findViewById(R.id.chart)
-        val headerTextView: TextView = findViewById(R.id.header)
 
-        val entries: List<PieEntry> = listOf(
-            PieEntry(29.8f, "JavaScript"),
-            PieEntry(19.64f, "Python"),
-            PieEntry(17.78f, "Java"),
-            PieEntry(12.21f, "C#"),
-            PieEntry(9.38f, "PHP"),
-            PieEntry(9.14f, "C/C++"),
-            PieEntry(4.37f, "Ruby")
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(30f, "Категория 1"))
+        entries.add(PieEntry(20f, "Категория 2"))
+        entries.add(PieEntry(15f, "Категория 3"))
+        entries.add(PieEntry(10f, "Категория 4"))
+        entries.add(PieEntry(8f, "Категория 5"))
+        entries.add(PieEntry(7f, "Категория 6"))
+        entries.add(PieEntry(5f, "Категория 7"))
+        entries.add(PieEntry(5f, "Категория 8"))
+
+        val dataSet = PieDataSet(entries, "Распределение")
+
+        dataSet.colors = listOf(
+            Color.rgb(255, 0, 0),
+            Color.rgb(255, 165, 0),
+            Color.rgb(255, 255, 0),
+            Color.rgb(0, 255, 0),
+            Color.rgb(0, 0, 255),
+            Color.rgb(75, 0, 130),
+            Color.rgb(238, 130, 238),
+            Color.rgb(128, 128, 128)
         )
 
-        val dataSet = PieDataSet(entries, "Most Demanded Programming Languages")
-        dataSet.colors = ColorTemplate.COLORFUL_COLORS.toList()
-
         val data = PieData(dataSet)
+
         pieChart.data = data
-
-        pieChart.description.text = "Programming languages"
-        pieChart.isDrawHoleEnabled = true
-        pieChart.setHoleColor(android.R.color.white)
-        pieChart.setTransparentCircleColor(android.R.color.white)
-        pieChart.setTransparentCircleAlpha(110)
-
-        pieChart.animateXY(1400, 1400, Easing.EaseInOutQuad)
-
-        headerTextView.text = getString(R.string.most_demanded_programming_languages)
+        pieChart.description.isEnabled = false
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+        pieChart.animateXY(1400, 1400, Easing.EaseInOutQuad, Easing.EaseInOutQuad)
 
         pieChart.invalidate()
     }
